716c511e8769b3468d77ecbbc605b028f3fd2ef7
GilangIRSYD <58033323+GilangIRSYD@users.noreply.github.com>
2020-08-12 09:10:49 +0700

    add item click

---
+++ app/src/main/java/com/catatancodingku/kotlinrv/DetailAct.kt
@@ -0,0 +1,11 @@
+package com.catatancodingku.kotlinrv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
